e3cb3aed201d6e1a6871d86b3617fab4e42dfcb2
Razvan <negrea1razvan@yahoo.com>
2025-03-04 10:00:31 +0200

    Add main

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "labGitMPP1"
+
--- src/main/java/com/beginsecure/Main.java
+++ src/main/java/com/beginsecure/Main.java
@@ -1,4 +1,6 @@
--//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+package com.beginsecure;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
